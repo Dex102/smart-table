cddb82687c6da2d5a993174f9552002d5a89f558
Dex102 <dex.0990@yandex.ru>
2026-09-13 18:37:34 +0300

    Шаг № 3. Добавить сортировку

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import {processFormData} from "./lib/utils.js";
 import {initTable} from "./components/table.js";
 // @todo: подключение
 import {initPagination} from "./components/pagination.js";
+import {initSorting} from "./components/sorting.js";
 
 // Исходные данные используемые в render()
 const {data, ...indexes} = initData(sourceData);
@@ -37,7 +38,8 @@ function render(action) {
     let state = collectState(); // состояние полей из таблицы
     let result = [...data]; // копируем для последующего изменения
     // @todo: использование
-    result = applyPagination(result, state, action); 
+    result = applyPagination(result, state, action);
+    result = applySorting(result, state, action);
 
     sampleTable.render(result)
 }
@@ -45,7 +47,7 @@ function render(action) {
 const sampleTable = initTable({
     tableTemplate: 'table',
     rowTemplate: 'row',
-    before: [],
+    before: ['header'],
     after: ['pagination']
 }, render);
 
@@ -62,6 +64,10 @@ const applyPagination = initPagination(
     }
 );
 
+const applySorting = initSorting([        // Нам нужно передать сюда массив элементов, которые вызывают сортировку, чтобы изменять их визуальное представление
+    sampleTable.header.elements.sortByDate,
+    sampleTable.header.elements.sortByTotal
+]);
 
 const appRoot = document.querySelector('#app');
 appRoot.appendChild(sampleTable.container);
