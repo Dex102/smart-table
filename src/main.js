32b61207869b3e9955c9708b0103bacd8d37fa0c
Dex102 <dex.0990@yandex.ru>
2026-09-13 19:15:00 +0300

    Шаг № 5. Добавить поиск

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ import {initTable} from "./components/table.js";
 import {initPagination} from "./components/pagination.js";
 import {initSorting} from "./components/sorting.js";
 import {initFiltering} from "./components/filtering.js";
+import {initSearching} from "./components/searching.js";
 
 // Исходные данные используемые в render()
 const {data, ...indexes} = initData(sourceData);
@@ -42,6 +43,7 @@ function render(action) {
     result = applyPagination(result, state, action);
     result = applySorting(result, state, action);
     result = applyFiltering(result, state, action);
+    result = applySearching(result, state, action);
     
     
 
@@ -76,6 +78,10 @@ const applySorting = initSorting([        // Нам нужно передать 
 const applyFiltering = initFiltering(sampleTable.filter.elements, {    // передаём элементы фильтра
     searchBySeller: indexes.sellers                                    // для элемента с именем searchBySeller устанавливаем массив продавцов
 });
+
+const applySearching = initSearching ('search');
+
+
 
 const appRoot = document.querySelector('#app');
 appRoot.appendChild(sampleTable.container);
